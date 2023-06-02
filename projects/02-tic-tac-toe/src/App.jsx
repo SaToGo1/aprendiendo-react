086040652df50e1be2f5a7feb17ce395a9ceb5f6
SaToGo1 <stoledano97@gmail.com>
2023-06-02 20:19:36 +0200

    Muestra tablero, Añade Componente Square

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,9 +1,41 @@
 import './App.css'
+import { useState } from 'react';
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+};
+
+// const board = Array(9).fill(null);
+
+const Square = ({ children, updateBoard, index}) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
 
 function App() {
-
+  const [board, setBoard] = useState(Array(9).fill(null));
   return (
-    <h1>Tic tac toe</h1>
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+                
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
