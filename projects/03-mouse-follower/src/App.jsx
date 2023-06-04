d96d331008c696c1604c16855ef6bd6e38c2ecad
SaToGo1 <stoledano97@gmail.com>
2023-06-04 20:02:41 +0200

    Añade Evento de tracking para el raton con useEffect,
    Añade boton para activar y esactivar el evento de tracking

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,11 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log('effect', { enabled });
+    
+    // TRACKING DEL MOUSE
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log('handleMove', { clientX, clientY});
+      setPosition({ x: clientX, y: clientY });
+    }
+    // Only track if enabled == true
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    
+    // cleanup
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <h3>Proyecto 3</h3>  
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+      }} 
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+ 
   )
 }
 
