06ea39cde98730bd071446c75b6c2db970b514bc
SaToGo1 <stoledano97@gmail.com>
2023-06-05 21:29:31 +0200

    Aplicación v2

--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -1,31 +1,39 @@
 import { useState, useEffect } from 'react'
+import './styles/App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export const App = () => {
   const [fact, setFact] = useState('')
-  const [imgSrc, setImgSrc] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
 
   useEffect(() => {
-    // Fetching of the fact
-    fetch('https://catfact.ninja/fact')
-      .then(data => data.json())
-      .then(factObj => {
-        // save fact in state
-        setFact(factObj.fact)
-        return factObj.fact
-      })
-    // Getting first Word
-      .then(fact => fact.split(' ')[0])
-    // fetching image with first word
-      .then(firstWord => {
-        setImgSrc(`https://cataas.com/cat/says/${firstWord}`)
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWords)
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            console.log(response)
+            const { url } = response
+            setImageUrl(url)
+          })
       })
   }, [])
 
   return (
-    <>
+    <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
-      <img src={imgSrc} alt='imagen de gatito' />
-    </>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
   )
 }
