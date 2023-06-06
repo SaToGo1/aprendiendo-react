67a27d613b35b03d5be98fb722e6f35f4745847e
SaToGo1 <stoledano97@gmail.com>
2023-06-06 21:55:30 +0200

    Añadido botton para cambiar la anecdota de gatos

--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -8,24 +8,18 @@ const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 export const App = () => {
   const [fact, setFact] = useState('')
   const [imageUrl, setImageUrl] = useState('')
-  const [factError, setFactError] = useState()
 
-  // para recuperar la cita al cargar la página
-  useEffect(() => {
+  const getRandomFact = () => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => {
-        // if (!res.ok) {
-        //   setFactError('No se ha podido recuperar la cita')
-        // }
         if (!res.ok) throw new Error('Error fetching fact')
         return res.json()
       })
       .then(data => setFact(data.fact))
-      // .catch((err) => {
-      //   // tanto si hay un error con la respuesta
-      //   // como si hay un error con la petición
-      // })
-  }, [])
+  }
+
+  // para recuperar la cita al cargar la página
+  useEffect(getRandomFact, [])
 
   // para recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
@@ -46,8 +40,13 @@ export const App = () => {
   return (
     <main>
       <h1>App de gatitos</h1>
+      <button onClick={getRandomFact}>Change Fact</button>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+      {imageUrl &&
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />}
     </main>
   )
 }
