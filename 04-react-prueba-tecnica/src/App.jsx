d98249968a1d1a617c7844a97b81e842c2cd2fba
SaToGo1 <stoledano97@gmail.com>
2023-06-06 22:09:43 +0200

    Lógica de fact separada del componente

--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import './styles/App.css'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { getRandomFact } from './services/facts'
+
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
@@ -9,18 +10,15 @@ export const App = () => {
   const [fact, setFact] = useState('')
   const [imageUrl, setImageUrl] = useState('')
 
-  const getRandomFact = () => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => {
-        if (!res.ok) throw new Error('Error fetching fact')
-        return res.json()
-      })
-      .then(data => setFact(data.fact))
-  }
-
   // para recuperar la cita al cargar la página
-  useEffect(getRandomFact, [])
+  useEffect(() => {
+    getRandomFact().then(data => setFact(data))
+  }, [])
 
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
   // para recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
     if (!fact) return
@@ -40,7 +38,7 @@ export const App = () => {
   return (
     <main>
       <h1>App de gatitos</h1>
-      <button onClick={getRandomFact}>Change Fact</button>
+      <button onClick={handleClick}>Change Fact</button>
       {fact && <p>{fact}</p>}
       {imageUrl &&
         <img
