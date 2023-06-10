464eeba87fbf649eb62c21970f824f68c75d407a
SaToGo1 <stoledano97@gmail.com>
2023-06-10 02:59:35 +0200

    Refactorización de la lista de películas

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -2,9 +2,10 @@ import './App.css'
 import responseMovies from './mock/with-results.json'
 import withoutResults from './mock/no-results.json'
 
+import { Movies } from './components/Movies'
+
 function App() {
   const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
 
   return (
     <div className='app'>
@@ -16,23 +17,7 @@ function App() {
         </form>
       </header>
       <main>
-        {
-          hasMovies
-            ? (
-              <ul>
-                {movies.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title} />
-                  </li>
-                ))}
-              </ul>
-            )
-            : (
-              <h3>No se encontraron resultados</h3>
-            )
-        }
+        <Movies movies={movies}/>
       </main>
     </div>
   )
