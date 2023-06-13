7bfea92d4a55822f052d204e15acb94dfb1cfea6
SaToGo1 <stoledano97@gmail.com>
2023-06-13 11:25:36 +0200

    Convierte la lógica de la búsqueda en un custom Hook,
    Añade grid responsive para las películas

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -3,10 +3,41 @@ import { useEffect, useRef, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
 function App() {
   const { movies } = useMovies()
-  const [query, setQuery] = useState('')
-  const [error, setError] = useState(null)
+  const { search, updateSearch, error } = useSearch()
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -14,34 +45,24 @@ function App() {
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value)
+    updateSearch(event.target.value)
   }
-
-  useEffect(() => {
-    if (query === '') {
-      setError('No se puede buscar una película vacía')
-      return
-    }
-
-    if (query.match(/^\d+$/)) {
-      setError('No se puede buscar una película con un número')
-      return
-    }
-
-    if (query.length < 3) {
-      setError('La búsqueda debe tener al menos 3 caracteres')
-      return
-    }
-
-    setError(null)
-  }, [query])
 
   return (
     <div className='app'>
       <header>
         <h1>Buscador de Películas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars' />
+          <input 
+            style={{
+              border: '1px solid transparent', 
+              borderColor: error ? 'red' : 'transparent'
+              }} 
+            onChange={handleChange} 
+            value={search} 
+            name='query' 
+            placeholder='Avengers, Star Wars' 
+          />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
