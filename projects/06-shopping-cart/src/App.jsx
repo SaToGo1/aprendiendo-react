45b3ffab6b81497cb874d6243fb42d3076f3000e
SaToGo1 <stoledano97@gmail.com>
2023-06-15 22:23:32 +0200

    Proyecto 6 shopping cart

---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+    </>
+  )
+}
+
+export default App
