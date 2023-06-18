6eea1d2bee110055aebec10f767e7c9882102d2b
SaToGo1 <stoledano97@gmail.com>
2023-06-19 01:04:03 +0200

    cart context

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -4,21 +4,26 @@ import { products as initialProducts } from './mock/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { Cart } from './components/Cart'
 
 // Custom Hooks
 import { useFilters } from './hooks/useFilters'
 
+// Context
+import { CartProvider } from './context/cart'
+
 function App () {
-  const { filters, filterProducts } = useFilters()
+  const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       <Footer />
-    </>
+    </CartProvider>
   )
 }
 
---
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from 'react'
+
+// 1.crear contexto
+export const cartContext = createContext()
+
+// 2. crear  provider
+export function CartProvider ({ children }) {
+  const [cart, setCart] = useState([])
+
+  const addToCart = product => {
+    // check if product already in car.
+    const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+    if (productInCartIndex >= 0) {
+      // una forma seria usando structuredClone
+      const newCart = structuredClone(cart)
+      newCart[productInCartIndex].quantity += 1
+      return setCart(newCart)
+    }
+
+    // producto no está en el carrito
+    setCart(prevState => ([
+      ...prevState,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]))
+  }
+
+  const clearCart = () => {
+    setCart([])
+  }
+
+  return (
+    <cartContext.Provider value={{
+      cart,
+      addToCart,
+      clearCart
+    }}
+    >
+      {children}
+    </cartContext.Provider>
+  )
+}
